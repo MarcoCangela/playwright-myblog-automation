61e800b1c8b3609f4d11cf7bf0c946ce9f86c9ef
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-11-28 22:54:51 +0200

    Cleaning up

--- tests/Testes.spec.js
+++ tests/Testes.spec.js
@@ -29,10 +29,7 @@ test("Access Blog and access the first blog post", async ({page}) => {
   // Take a screenshot after the test
   await page.screenshot({ path: 'screenshot.png' });
 
-  //When the step above is executed, a new page will be opened and in order to get to the new page we must follow the steps bellow 
-  // We wait for the event to be triggered
-  // When we click on the first blog post, a new page will be opened,
-  // so we wait for the "popup" event to be triggered
+  // When we click on the first blog post, a new page will be opened, so we wait for the "popup" event to be triggered
   const newTabPromise = page.waitForEvent("popup");
   // We get the new page as a promise 
   const newTab = await newTabPromise;
@@ -41,16 +38,12 @@ test("Access Blog and access the first blog post", async ({page}) => {
   await newTab.evaluate(() => {
     window.scrollBy(0, -500); // Scroll up by 500 pixels
   });
-  // await expect(newTab).toHaveURL("");
+  //Ensuring we are on the correct page
   expect(await newTab.title()).toBe("Beyond Functional Testing: The Value of Non-Functional Testing | by Marco Garujo | Nov, 2024 | Medium");
 
 
 });
 
 test.only('Implicetly Opening a new page', async ({page}) => {
-
-
-
-
 
 })
