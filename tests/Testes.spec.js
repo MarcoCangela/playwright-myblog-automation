be49b8338b76b04b44009117ac65d5ce8178309d
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-12-06 23:25:01 +0200

    Code Improvements

--- tests/Testes.spec.js
+++ tests/Testes.spec.js
@@ -45,12 +45,13 @@ test("Access Blog and access the first blog post", async ({page}) => {
 });
 
 test('Implicetly Opening a new page', async ({page}) => {
-  const newPagePromise = page.waitForEvent('popup');
-  await page.getByRole('link', { name: ' My LinkedIn' }).click({
+  //Get the new page that will be opened with the clink on the link 
+  // const newPagePromise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'My LinkedIn' }).click({
     modifiers: ['ControlOrMeta']
   });
-  const newPage = await newPagePromise;
-  await newPage.waitForLoadState('networkidle');
+  // const newPage = await newPagePromise;
+  // await newPage.waitForLoadState('networkidle');
 
   await page.getByRole('link', { name: 'My GitHub' }).click({
     modifiers: ['ControlOrMeta']
